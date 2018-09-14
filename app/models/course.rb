480bf8ae8494f6762fac17ea6dba03b51df2d1cb
Kevin Lee <40494894+kevinlee6@users.noreply.github.comw>
2018-09-14 12:22:55 -0400

    course 32bit validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,5 +2,5 @@ class Course < ApplicationRecord
   has_many :cohorts, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
-  validates :class_hours, numericality: { greater_than_or_equal_to: 0 }
+  validates :class_hours, numericality: { greater_than_or_equal_to: 0, less_than: 2^31 }
 end
