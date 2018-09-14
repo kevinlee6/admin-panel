a4cd9acc05605e24d66a2d5657388873b07ce7d1
Kevin Lee <40494894+kevinlee6@users.noreply.github.comw>
2018-09-14 11:19:28 -0400

    dependent destroy course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,5 +1,5 @@
 class Course < ApplicationRecord
-  has_many :cohorts
+  has_many :cohorts, dependent: :destroy
 
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :class_hours, numericality: { greater_than_or_equal_to: 0 }
