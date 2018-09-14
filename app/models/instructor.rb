22d1544049ddcd12bc30f20a4a1c8bc2391fcfd9
Kevin Lee <40494894+kevinlee6@users.noreply.github.comw>
2018-09-14 12:19:38 -0400

    instructor salary must be less than 32bit

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -9,6 +9,6 @@ class Instructor < ApplicationRecord
   validates :first_name, :last_name, :education, presence: true
   validates :education, inclusion: { in: Instructor.education }
   validates :age, inclusion: { in: 0...150 }
-  validates :salary, numericality: { greater_than_or_equal_to: 0 }
+  validates :salary, numericality: { greater_than_or_equal_to: 0, less_than: 2**31 }
   validates_uniqueness_of :cohort_id, allow_blank: true
 end
