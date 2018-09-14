6f30eb21bb893f2ad584dfbb51ca6f9af0e5ca62
Kevin Lee <40494894+kevinlee6@users.noreply.github.comw>
2018-09-14 11:56:37 -0400

    dependent nullify instructors with cohorts

--- app/models/cohort.rb
+++ app/models/cohort.rb
@@ -1,6 +1,6 @@
 class Cohort < ApplicationRecord
   belongs_to :course
-  has_one :instructor
+  has_one :instructor, dependent: :nullify
   has_many :cohort_students
   has_many :students, through: :cohort_students
 
