5be16c8b424f13baa18be08b25eb3ce9dcfaecdd
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-01-31 13:40:41 -0500

    Add to cohort unit test.

--- spec/models/cohort_spec.rb
+++ spec/models/cohort_spec.rb
@@ -13,50 +13,54 @@ RSpec.describe Cohort, type: :model do
   context 'validation tests' do
     before do
       Course.new(name: 'Test', class_hours: 10).save
+      params[:course_id] = Course.last.id;
     end
 
     it 'ensures that it can be saved' do
-      mod_params = params.clone
-      mod_params[:course_id] = Course.last.id
-      cohort = Cohort.new(mod_params).save
+      cohort = Cohort.new(params).save
       expect(cohort).to eq true
     end
 
     it 'ensures that it must belongs_to course' do
       mod_params = params.clone
-      mod_params[:course_id] = Course.last.id
       mod_params.delete(:course_id)
       cohort = Cohort.new(mod_params).save
       expect(cohort).to eq false
     end
 
-    # it 'ensures class_hours presence' do
-    #   course = Course.new(name: 'Test').save
-    #   expect(course).to eq (false)
-    # end
+    it 'ensures that it must course it belongs_to must be valid' do
+      new_course_id = { course_id: 0 } 
+      mod_params = params.merge(new_course_id)
+      cohort = Cohort.new(mod_params).save
+      expect(cohort).to eq false
+    end
 
-    # it 'ensures class_hours cannot be negative' do
-    #   course = Course.new(name: 'Test', class_hours: -20).save
-    #   expect(course).to eq (false)
-    # end
+    it 'ensures start_date cannot be greater than end_date' do
+      new_dates = { start_date: DateTime.now, end_date: DateTime.now - 5 }
+      mod_params = params.merge(new_dates)
+      cohort = Cohort.new(mod_params).save
+      expect(cohort).to eq false
+    end
 
-    # it 'ensures class_hours cannot exceed 32-bit integer limit' do
-    #   course = Course.new(name: 'Test', class_hours: 2**31).save
-    #   expect(course).to eq (false)
-    # end
+    it 'ensures name must exist' do
+      mod_params = params.clone
+      mod_params.delete(:name)
+      cohort = Cohort.new(mod_params).save
+      expect(cohort).to eq false
+    end
 
-    # it 'ensures names are unique (not case sensitive)' do
-    #   name = 'Test'
-    #   name2 = 'test'
-    #   course1 = Course.new(name: name, class_hours: 20).save
-    #   course2 = Course.new(name: name, class_hours: 20).save
-    #   expect(course1).to eq (true)
-    #   expect(course2).to eq (false)
-    # end
+    it 'ensures start_date must exist' do
+      mod_params = params.clone
+      mod_params.delete(:start_date)
+      cohort = Cohort.new(mod_params).save
+      expect(cohort).to eq false
+    end
 
-    # it 'ensures saves correctly' do
-    #   course = Course.new(name: 'Test', class_hours: 20).save
-    #   expect(course).to eq (true)
-    # end
+    it 'ensures end_date must exist' do
+      mod_params = params.clone
+      mod_params.delete(:end_date)
+      cohort = Cohort.new(mod_params).save
+      expect(cohort).to eq false
+    end
   end
 end
