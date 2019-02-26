9ad69d82b4ab5fd8031407bd49ee2a0848551ca0
Kevin Lee <40494894+kevinlee6@users.noreply.github.com>
2019-02-26 14:10:32 -0500

    Main content expands to fit ddevice width; regarding sidebar toggle.

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,5 +1,6 @@
 $(document).on('turbolinks:load', () => {
   $('.navbar-menu-btn').click(() => {
     $('#left-navbar-container').toggle('fast');
+    $('.main-content').toggleClass('col-md-10');
   });
 });
